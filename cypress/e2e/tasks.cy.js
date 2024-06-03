c347839d8785658648ada3c527b36229eb8f077d
vique <victoriarduarte@hotmail.com>
2024-06-03 16:56:38 -0300

    Add test: should not create duplicate task

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -21,4 +21,35 @@ describe('tasks', () => {
         cy.contains('main div p', 'Read a node.js book')
             .should('be.visible')
     });
+
+    it('should not create duplicate task', () => {
+
+        // Delete task
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: { name: 'Study Javascript' }
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        // Create task
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: { name: 'Study Javascript', is_done: false }
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        cy.visit('http://localhost:3000')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Study Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        cy.contains('.swal2-html-container', 'Task already exists!')
+            .should('be.visible')
+    });
 })
