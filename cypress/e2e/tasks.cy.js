ccb3b70a4e939c82fcfa7b268c9f11c4d5f2c018
vique <victoriarduarte@hotmail.com>
2024-06-04 10:43:07 -0300

    Add context: create

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,38 +1,42 @@
 /// <reference types="cypress" />
 
 describe('tasks', () => {
-    it('should create a new task', () => {
 
-        const taskName = 'Read a node.js book'
+    context('create', () => {
+        it('should create a new task', () => {
 
-        cy.removeTaskByName(taskName)
+            const taskName = 'Read a node.js book'
+    
+            cy.removeTaskByName(taskName)
+    
+            cy.createTask(taskName)
+    
+            cy.contains('main div p', taskName)
+                .should('be.visible')
+        });
+    
+        it('should not create duplicate task', () => {
+    
+            const task = {
+                name: 'Study Javascript',
+                is_done: false
+            }
+    
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+    
+            cy.createTask(task.name)
+    
+            cy.contains('.swal2-html-container', 'Task already exists!')
+                .should('be.visible')
+        });
+    
+        it('required field', () => {
+    
+            cy.createTask()
+    
+            cy.isRequired('This is a required field')
+        });
+    })
 
-        cy.createTask(taskName)
-
-        cy.contains('main div p', taskName)
-            .should('be.visible')
-    });
-
-    it('should not create duplicate task', () => {
-
-        const task = {
-            name: 'Study Javascript',
-            is_done: false
-        }
-
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-
-        cy.createTask(task.name)
-
-        cy.contains('.swal2-html-container', 'Task already exists!')
-            .should('be.visible')
-    });
-
-    it('required field', () => {
-
-        cy.createTask()
-        
-        cy.isRequired('This is a required field')
-    });
 })
