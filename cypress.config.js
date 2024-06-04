f2821e9ffea2b3c5d9d4bc7dfb58c2a7678055d1
vique <victoriarduarte@hotmail.com>
2024-06-04 19:28:44 -0300

    Update viewport config

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     baseUrl: 'http://localhost:3000',
     env: {
       apiUrl: 'http://localhost:3333'
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
